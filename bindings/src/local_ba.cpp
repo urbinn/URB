8a4f7a61c2c0229eb0fce01588dbeb1ab4b8395f
Daniello <d_crush@hotmail.com>
2017-12-14 17:28:44 +0100

    BA 18 column fix

--- bindings/src/local_ba.cpp
+++ bindings/src/local_ba.cpp
@@ -63,10 +63,10 @@ Eigen::MatrixXd keyFrameRowToMatrix(const Eigen::MatrixXd row)
 {
     Eigen::MatrixXd frameMtrix(4, 4);
     
-    frameMtrix << row(1), row(2), row(3), row(4),
-    row(5), row(6), row(7), row(8),
-    row(9), row(10), row(11), row(12),
-    row(13), row(14), row(15), row(16);
+    frameMtrix << row(2), row(3), row(4), row(5),
+    row(6), row(7), row(8), row(9),
+    row(10), row(11), row(12), row(13),
+    row(14), row(15), row(16), row(17);
     
     return frameMtrix;
 }
