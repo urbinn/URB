3048066542486425b15e8aa689b0f22ac29e7b42
Daniello <d_crush@hotmail.com>
2017-12-22 12:54:20 +0100

    full ba import remove duplicate 1

--- bindings/src/full_ba.cpp
+++ bindings/src/full_ba.cpp
@@ -9,117 +9,116 @@
 #include <opencv2/core/core.hpp>
 #include <Eigen/StdVector>
 
-#include "local_ba.h"
-#include "local_ba.cpp"
+#include "full_ba.h"
 
 using namespace std;
 
-// // column size for keyframe
-// typedef Eigen::Matrix<double, 4, 4> KeyFrameMatrix;
-// typedef Eigen::Matrix<double, 1, 3> MapPointMatrix;
-// typedef Eigen::Matrix<double, 1, 4> KeyFrameMapPointMatrix;
+// column size for keyframe
+typedef Eigen::Matrix<double, 4, 4> KeyFrameMatrix;
+typedef Eigen::Matrix<double, 1, 3> MapPointMatrix;
+typedef Eigen::Matrix<double, 1, 4> KeyFrameMapPointMatrix;
 
-// //index - key
-// typedef std::pair<  std::pair<int, int>, KeyFrameMatrix> KeyFrame;
-// typedef std::pair< std::pair<int, int>, MapPointMatrix> MapPoint;
+//index - key
+typedef std::pair<  std::pair<int, int>, KeyFrameMatrix> KeyFrame;
+typedef std::pair< std::pair<int, int>, MapPointMatrix> MapPoint;
 
-// const float CameraFx = 718.856;
-// const float CameraFy = 718.856;
-// const float CameraCx = 607.1928;
-// const float CameraCy = 185.2157;
+const float CameraFx = 718.856;
+const float CameraFy = 718.856;
+const float CameraCx = 607.1928;
+const float CameraCy = 185.2157;
 
-// Eigen::Matrix<double,3,1> toVector3d(const MapPointMatrix point){
-//     Eigen::Matrix<double,3,1> v;
-//     v << point(0), point(1), point(2);
+Eigen::Matrix<double,3,1> toVector3d(const MapPointMatrix point){
+    Eigen::Matrix<double,3,1> v;
+    v << point(0), point(1), point(2);
     
-//     return v;
-// }
+    return v;
+}
 
-// g2o::SE3Quat toSE3QuatFromMatrix(const Eigen::Matrix<double,4,4> &eigenMat){
-//     Eigen::Matrix<double,3,3> R;
-//     R << eigenMat(0,0), eigenMat(0,1), eigenMat(0,2),
-//     eigenMat(1,0), eigenMat(1,1), eigenMat(1,2),
-//     eigenMat(2,0), eigenMat(2,1), eigenMat(2,2);
+g2o::SE3Quat toSE3QuatFromMatrix(const Eigen::Matrix<double,4,4> &eigenMat){
+    Eigen::Matrix<double,3,3> R;
+    R << eigenMat(0,0), eigenMat(0,1), eigenMat(0,2),
+    eigenMat(1,0), eigenMat(1,1), eigenMat(1,2),
+    eigenMat(2,0), eigenMat(2,1), eigenMat(2,2);
     
-//     Eigen::Matrix<double,3,1> t(eigenMat(0,3), eigenMat(1,3), eigenMat(2,3));
+    Eigen::Matrix<double,3,1> t(eigenMat(0,3), eigenMat(1,3), eigenMat(2,3));
     
-//     return g2o::SE3Quat(R,t);
-// }
+    return g2o::SE3Quat(R,t);
+}
 
-// Eigen::MatrixXd toEigenBundel(const g2o::SE3Quat &SE3){
-//     Eigen::Matrix<double, 4, 4> eigMat = SE3.to_homogeneous_matrix();
-//     return eigMat;
-// }
-
-
-// Eigen::MatrixXd toEigenVector(const Eigen::Matrix<double,3,1> &m)
-// {
-//     Eigen::Matrix<double, 1, 3> eigMat;
-//     eigMat << m(0,0), m(1,0), m(2,0);
-//     return eigMat;
-// }
+Eigen::MatrixXd toEigenBundel(const g2o::SE3Quat &SE3){
+    Eigen::Matrix<double, 4, 4> eigMat = SE3.to_homogeneous_matrix();
+    return eigMat;
+}
 
 
-// Eigen::MatrixXd keyFrameRowToMatrix(const Eigen::MatrixXd row)
-// {
-//     Eigen::MatrixXd frameMtrix(4, 4);
+Eigen::MatrixXd toEigenVector(const Eigen::Matrix<double,3,1> &m)
+{
+    Eigen::Matrix<double, 1, 3> eigMat;
+    eigMat << m(0,0), m(1,0), m(2,0);
+    return eigMat;
+}
+
+
+Eigen::MatrixXd keyFrameRowToMatrix(const Eigen::MatrixXd row)
+{
+    Eigen::MatrixXd frameMtrix(4, 4);
     
-//     frameMtrix << row(1), row(2), row(3), row(4),
-//     row(5), row(6), row(7), row(8),
-//     row(9), row(10), row(11), row(12),
-//     row(13), row(14), row(15), row(16);
+    frameMtrix << row(1), row(2), row(3), row(4),
+    row(5), row(6), row(7), row(8),
+    row(9), row(10), row(11), row(12),
+    row(13), row(14), row(15), row(16);
     
-//     return frameMtrix;
-// }
+    return frameMtrix;
+}
 
-// Eigen::MatrixXd mappointRowToMatrix(const Eigen::MatrixXd row)
-// {
-//     Eigen::MatrixXd pointMatrix(1, 3);
-//     pointMatrix << row(1), row(2), row(3) ;
-//     return pointMatrix;
-// }
+Eigen::MatrixXd mappointRowToMatrix(const Eigen::MatrixXd row)
+{
+    Eigen::MatrixXd pointMatrix(1, 3);
+    pointMatrix << row(1), row(2), row(3) ;
+    return pointMatrix;
+}
 
 
-// std::vector<KeyFrame> getObservations(MapPoint point ,  std::vector<KeyFrame> keyframes,  Eigen::MatrixXd worldMapPoints,Eigen::MatrixXd pointsRelation) {
-//     std::vector<KeyFrame> returnKeyframes;
-//     for(int r = 0; r <  pointsRelation.rows(); r++) {
-//         Eigen::MatrixXd currentRelation(1, pointsRelation.cols());
-//         currentRelation << pointsRelation.row(r);
-//         if (point.first.second == currentRelation(0)) {
-//             //found relation
-//             for (KeyFrame frame : keyframes) {
-//                 if (currentRelation(1) == frame.first.second) {
-//                     returnKeyframes.push_back(frame);
-//                     break;
-//                 }
-//             }
-//         }
-//     }
+std::vector<KeyFrame> getObservations(MapPoint point ,  std::vector<KeyFrame> keyframes,  Eigen::MatrixXd worldMapPoints,Eigen::MatrixXd pointsRelation) {
+    std::vector<KeyFrame> returnKeyframes;
+    for(int r = 0; r <  pointsRelation.rows(); r++) {
+        Eigen::MatrixXd currentRelation(1, pointsRelation.cols());
+        currentRelation << pointsRelation.row(r);
+        if (point.first.second == currentRelation(0)) {
+            //found relation
+            for (KeyFrame frame : keyframes) {
+                if (currentRelation(1) == frame.first.second) {
+                    returnKeyframes.push_back(frame);
+                    break;
+                }
+            }
+        }
+    }
     
-//     return returnKeyframes;
-// }
+    return returnKeyframes;
+}
 
-// std::vector<std::pair<KeyFrame, int>> getObservationsWithRelation(MapPoint point ,  std::vector<KeyFrame> keyframes,  Eigen::MatrixXd, Eigen::MatrixXd pointsRelation ) {
-//     std::vector<std::pair<KeyFrame, int>> returnKeyframes;
-//     for(int r = 0; r <  pointsRelation.rows(); r++) {
-//         Eigen::MatrixXd currentRelation(1, pointsRelation.cols());
-//         currentRelation << pointsRelation.row(r);
-//         //cout << currentRelation << endl;
-//         //cout << point.first.second << endl;
-//         if (point.first.second == currentRelation(0)) {
-//             //found relation
-//             for (KeyFrame frame : keyframes) {
-//                 if (currentRelation(1) == frame.first.second) {
+std::vector<std::pair<KeyFrame, int>> getObservationsWithRelation(MapPoint point ,  std::vector<KeyFrame> keyframes,  Eigen::MatrixXd, Eigen::MatrixXd pointsRelation ) {
+    std::vector<std::pair<KeyFrame, int>> returnKeyframes;
+    for(int r = 0; r <  pointsRelation.rows(); r++) {
+        Eigen::MatrixXd currentRelation(1, pointsRelation.cols());
+        currentRelation << pointsRelation.row(r);
+        //cout << currentRelation << endl;
+        //cout << point.first.second << endl;
+        if (point.first.second == currentRelation(0)) {
+            //found relation
+            for (KeyFrame frame : keyframes) {
+                if (currentRelation(1) == frame.first.second) {
                     
-//                     returnKeyframes.push_back(std::make_pair(frame,r) );
-//                     break;
-//                 }
-//             }
-//         }
-//     }
+                    returnKeyframes.push_back(std::make_pair(frame,r) );
+                    break;
+                }
+            }
+        }
+    }
     
-//     return returnKeyframes;
-// }
+    return returnKeyframes;
+}
 
 int fullBundleAdjustment(Eigen::Ref<Eigen::MatrixXd> keyframes, Eigen::Ref<Eigen::MatrixXd> mapPoints, Eigen::Ref<Eigen::MatrixXd> pointsRelation )  {
        //step 1 setup keyframes
