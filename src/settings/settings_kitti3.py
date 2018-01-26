6a4c82955b90e6f7770431c1353cabd7b31d2b0b
Jeroen Vuurens <jbpvuurens@gmail.com>
2018-01-26 12:16:38 +0100

    settings

---
+++ src/settings/settings_kitti3.py
@@ -0,0 +1,12 @@
+from src.settings.settings import *
+
+# bf is baseline * focal distance om de diepte te berekenen met formule focal distance * baseline / disparity
+###KITTI
+CAMERA_BF= 387.5744
+CAMERA_FX = 721.5377
+CAMERA_FY = 721.5377
+CAMERA_CX = 609.5593
+CAMERA_CY = 172.854
+
+CAMERA_FX_INV = 1.0 / CAMERA_FX
+CAMERA_FY_INV = 1.0 / CAMERA_FY
---
+++ src/settings/settings_kitti4.py
@@ -0,0 +1,12 @@
+from src.settings.settings import *
+
+# bf is baseline * focal distance om de diepte te berekenen met formule focal distance * baseline / disparity
+###KITTI
+CAMERA_BF= 379.8145
+CAMERA_FX = 707.0912
+CAMERA_FY = 707.0912
+CAMERA_CX = 601.8873
+CAMERA_CY = 183.1104
+
+CAMERA_FX_INV = 1.0 / CAMERA_FX
+CAMERA_FY_INV = 1.0 / CAMERA_FY
